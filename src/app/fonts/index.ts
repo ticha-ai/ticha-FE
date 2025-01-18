a6f38d911e9ee3c1089edfc7c24ed3c0ebd53b25
Parksangwoo <psu8430@gmail.com>
2025-01-19 04:58:29 +0900

    style: 시스템 폰트 추가

---
+++ src/app/fonts/index.ts
@@ -0,0 +1,13 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  src: [
+    { path: './Pretendard-Regular.woff', weight: '400', style: 'normal' },
+    { path: './Pretendard-Medium.woff', weight: '500', style: 'normal' },
+    {
+      path: './Pretendard-SemiBold.woff',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+});
