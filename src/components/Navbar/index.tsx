f6b3ec174e5ebac6cb50bb4409d41459d2679e28
Parksangwoo <psu8430@gmail.com>
2025-01-31 17:14:09 +0900

    fix: 버튼 컴포넌트 분리에 따른 Navbar 내 버튼 활용 코드 수정

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,9 +3,10 @@
 import Image from 'next/image';
 
 import Logo from '@/../public/images/logo.png';
-import Button from '@/components/Button';
-import Icon from '@/components/Icon';
+import DefaultButton from '@/components/Button/DefaultButton';
+import TextButton from '@/components/Button/TextButton';
 
+import Icon from '../Icon';
 import style from './style.module.css';
 
 const Navbar = () => {
@@ -13,15 +14,13 @@ const Navbar = () => {
     <nav className={style.navbar}>
       <Image src={Logo} alt='logo' width={91.53} height={21.5} />
       <div className={style.navbarMenus}>
-        <Button size='gnb' isFull={false} color={'transparent'}>
-          티챠 소개
-        </Button>
-        <Button size='gnb' isFull={false}>
+        <TextButton size='gnb'>티챠 소개</TextButton>
+        <DefaultButton size='gnb' isFull={false}>
           문제지 생성
-        </Button>
-        <Button size='gnb' isFull={false} color={'transparent'}>
-          <Icon id={'user'} size='lg' className={style.userIcon} />
-        </Button>
+        </DefaultButton>
+        <TextButton size='gnb'>
+          <Icon id='user' size='lg' className={style.userIcon} />
+        </TextButton>
       </div>
     </nav>
   );
