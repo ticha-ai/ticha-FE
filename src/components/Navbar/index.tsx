5b88748f085c4950b0f9dd0d145574a01e598bb3
Parksangwoo <psu8430@gmail.com>
2025-02-01 17:19:35 +0900

    feat: Navbar내 소개 버튼 이벤트 추가

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,26 @@
 'use client';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 import Logo from '@/../public/images/logo.png';
 import DefaultButton from '@/components/Button/DefaultButton';
 import TextButton from '@/components/Button/TextButton';
+import ROUTE from '@/constants/route';
 
 import Icon from '../Icon';
 import style from './style.module.css';
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <nav className={style.navbar}>
       <Image src={Logo} alt='logo' width={91.53} height={21.5} />
       <div className={style.navbarMenus}>
-        <TextButton size='gnb'>티챠 소개</TextButton>
+        <TextButton size='gnb' onClick={() => router.push(ROUTE.intro)}>
+          티챠 소개
+        </TextButton>
         <DefaultButton size='gnb' isFull={false}>
           문제지 생성
         </DefaultButton>
