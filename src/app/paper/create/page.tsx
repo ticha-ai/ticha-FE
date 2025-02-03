6860d8b7f4d1e2bd2ef1161c945aeba9fe2702b2
Parksangwoo <psu8430@gmail.com>
2025-02-03 16:25:21 +0900

    feat: 문제지 생성 페이지 구현

---
+++ src/app/paper/create/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Fragment } from 'react';
+import { useStore } from 'zustand';
+
+import DefaultButton from '@/components/Button/DefaultButton';
+import OptionSection from '@/components/Paper/Create/OptionSection';
+import { PAPER_OPTIONS } from '@/constants/paperOption';
+import paperOptionStore from '@/store/paperOptionStore';
+
+import style from './style.module.css';
+
+const CreatePage = () => {
+  const { options, isFulfilled } = useStore(paperOptionStore);
+  const sumbitPaperOption = () => {
+    console.log(options);
+  };
+
+  return (
+    <div className={style.container}>
+      <div>
+        {PAPER_OPTIONS.map((option, index) => (
+          <Fragment key={index}>
+            <OptionSection {...option} />
+            {PAPER_OPTIONS.length - 1 !== index && (
+              <hr className={style.divider} />
+            )}
+          </Fragment>
+        ))}
+      </div>
+      <DefaultButton
+        isFull={false}
+        size='lg'
+        onClick={sumbitPaperOption}
+        disabled={!isFulfilled()}
+      >
+        <span className={style.button}>풀이 시작하기</span>
+      </DefaultButton>
+    </div>
+  );
+};
+
+export default CreatePage;
