b24048828f077e25a74df6e0eba89ad3b9dda09b
ohgus <123801385+ohgus@users.noreply.github.com>
2025-02-07 18:26:21 +0900

    refactor: user store에 profile_image 속성 추가

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -4,6 +4,7 @@ interface UserInfoProps {
   id: string;
   name: string;
   email: string;
+  profile_image: string;
 }
 
 interface UserInfoState {
@@ -19,12 +20,20 @@ const defaultState: UserInfoProps = {
   id: '',
   name: '',
   email: '',
+  profile_image: '',
 };
 
 const useUserInfo = create<UserInfoState & UserInfoActions>((set) => ({
   userInfo: defaultState,
   setUserInfo: (userInfo: UserInfoProps) => {
-    set({ userInfo });
+    set({
+      userInfo: {
+        id: userInfo.id,
+        name: userInfo.name,
+        email: userInfo.email,
+        profile_image: userInfo.profile_image,
+      },
+    });
   },
   clearUserInfo: () => {
     set({ userInfo: defaultState });
