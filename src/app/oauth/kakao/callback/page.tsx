4a8827cbea97c416b7dc0f29086d21690a326fec
ohgus <123801385+ohgus@users.noreply.github.com>
2025-02-07 18:27:56 +0900

    refactor: 로그인 성공시 유저 정보 저장, 토큰 저장 로직 추가

--- src/app/oauth/kakao/callback/page.tsx
+++ src/app/oauth/kakao/callback/page.tsx
@@ -4,12 +4,15 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
 import { kakaoLogin } from '@/apis/v1/auth/oauth';
+import useAuth from '@/hooks/useAuth';
+import useUserInfo from '@/hooks/useUserInfo';
 
-// 유저 정보를 저장하는 로직이 필요함
 const KakaoAuth = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
+  const { setUserInfo } = useUserInfo();
+  const { setTokens } = useAuth();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,12 +20,18 @@ const KakaoAuth = () => {
         if (!code) return;
 
         const response = await kakaoLogin({ code });
-        console.log(response.data);
+        const { access_token, refresh_token, user } = response.data;
+
+        setTokens({
+          accessToken: access_token,
+          refreshToken: refresh_token,
+        });
+        setUserInfo(user);
 
         // 나중에 로그인을 하면 어떤 페이지로 이동할지 결정이 필요함
         router.replace('/');
       } catch (error) {
-        console.warn(error);
+        console.warn('카카오 로그인 실패');
       }
     };
 
