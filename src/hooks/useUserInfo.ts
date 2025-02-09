2c6df8ccdd8cba3247e58a9baa6b8f19cfc73ddd
ohgus <123801385+ohgus@users.noreply.github.com>
2025-02-09 16:49:53 +0900

    refactor: persist 옵션 적용

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface UserInfoProps {
   id: string;
@@ -23,21 +24,28 @@ const defaultState: UserInfoProps = {
   profile_image: '',
 };
 
-const useUserInfo = create<UserInfoState & UserInfoActions>((set) => ({
-  userInfo: defaultState,
-  setUserInfo: (userInfo: UserInfoProps) => {
-    set({
-      userInfo: {
-        id: userInfo.id,
-        name: userInfo.name,
-        email: userInfo.email,
-        profile_image: userInfo.profile_image,
+const useUserInfo = create<UserInfoState & UserInfoActions>()(
+  persist(
+    (set) => ({
+      userInfo: defaultState,
+      setUserInfo: (userInfo: UserInfoProps) => {
+        set({
+          userInfo: {
+            id: userInfo.id,
+            name: userInfo.name,
+            email: userInfo.email,
+            profile_image: userInfo.profile_image,
+          },
+        });
       },
-    });
-  },
-  clearUserInfo: () => {
-    set({ userInfo: defaultState });
-  },
-}));
+      clearUserInfo: () => {
+        set({ userInfo: defaultState });
+      },
+    }),
+    {
+      name: 'userInfoStore',
+    },
+  ),
+);
 
 export default useUserInfo;
